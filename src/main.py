7df4f0fe65851022b633a6ae94ab78d9b1337aa2
Ekuspreet <ekuspreetsingh2@gmail.com>
2023-04-12 23:13:18 +0530

    sql4

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 #link db
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite4"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite3"
 db = SQLAlchemy(app)
 
 @app.route('/login/')
