e84452874c2fbd595eb16c3e7b37569203ee62da
DaveyDark <devesh97531@gmail.com>
2023-04-14 01:22:27 +0530

    Added login form and database tables

--- src/main.py
+++ src/main.py
@@ -8,7 +8,30 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite3"
 db = SQLAlchemy(app)
 
-@app.route('/login/')
+#make database tables
+class Customer(db.Model):
+    __tablename__ = 'customers'
+    customer_id = db.Column(db.String(100),unique=True,primary_key=True,nullable=False)
+    organisation_id = db.Column(db.String(100),nullable=False)
+    room_number = db.Column(db.String(100),nullable=False)
+    name = db.Column(db.String(100),nullable=False)
+
+    def __init__(self,customer_id,organisation_id,room_number,name):
+        self.customer_id = customer_id
+        self.organisation_id = organisation_id
+        self.room_number = room_number
+        self.name = name
+
+class Manager(db.Model):
+    __tablename__ = 'managers'
+    organisation_id = db.Column(db.String(100),unique=True,primary_key=True,nullable=False)
+    password = db.Column(db.String(100),nullable=False)
+
+    def __init__(self,organisation_id,password):
+        self.organisation_id = organisation_id
+        self.password = password
+
+@app.route('/login/',methods=["GET","POST"])
 def login():
     #login page
     return render_template('login.html')
@@ -26,5 +49,3 @@ if __name__ == '__main__':
         print("=> Database created")
     app.run(debug=True)
     print("=> Unmessed is running")
-
-
